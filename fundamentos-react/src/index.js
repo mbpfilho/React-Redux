21ead9a049a6c3d20694e9eb8f9d6c4dace935e3
Marcio Bueno <mabuen@live.com>
2022-12-29 22:42:33 +0100

    componentes com parametros

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import React from "react"
 
 import Primeiro from "./components/basics/Primeiro"
+import ComParametro from "./components/basics/ComParametro"
 
 const el=document.getElementById("root")
 const tag=<strong>Olá React!!!</strong>
@@ -10,5 +11,7 @@ ReactDOM.render(
   <div>
     {tag}
     <Primeiro></Primeiro>
+    <ComParametro titulo="Situacao do aluno" aluno="Pedro" nota={9.3}/>
+    <ComParametro titulo="Situacao do aluno" aluno="Maria" nota={9.9}/>
   </div>,
   el)
