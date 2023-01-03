7dff1b068acbc3e44bfa299f3b2cea1ab48d6241
Marcio Bueno <mabuen@live.com>
2023-01-03 11:40:32 +0100

    card

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,17 +1,29 @@
 import React from "react"
 
-import Primeiro from "./components/basics/Primeiro"
-import ComParametro from "./components/basics/ComParametro"
-import Fragmento from "./components/basics/Fragmento"
+import Card from "./components/layout/Card"
 import Aleatorio from "./components/basics/Aleatorio"
+import Fragmento from "./components/basics/Fragmento"
+import ComParametro from "./components/basics/ComParametro"
+import Primeiro from "./components/basics/Primeiro"
 
 export default _ =>(
   <div id="app">
     <h1>Fundamentos React</h1>
-    <Aleatorio min={2} max={20}/>
-    <Fragmento />
-    <ComParametro titulo="Situacao do aluno" aluno="Pedro" nota={9.3} />
-    <ComParametro titulo="Situacao do aluno" aluno="Maria" nota={9.9} />
-    <Primeiro></Primeiro>
+
+    <Card titulo="#4 - Desafio Aleatório">
+      <Aleatorio min={1} max={60}/>
+    </Card>
+
+    <Card titulo="#3 - Fragmento">
+      <Fragmento />
+    </Card>
+
+    <Card titulo="#2 - Com Parâmetro">
+      <ComParametro titulo="Situacao do aluno" aluno="Pedro" nota={9.3} />
+    </Card>
+
+    <Card titulo="#1 - Primeiro Componente">
+      <Primeiro></Primeiro>
+    </Card>
   </div>
 );
--- fundamentos-react/src/components/basics/Aleatorio.jsx
+++ fundamentos-react/src/components/basics/Aleatorio.jsx
@@ -1,7 +1,15 @@
 import React from "react"
 
-export default (props)=>{
-  let min = Math.ceil(props.min);
-  let max = Math.floor(props.max);
-  return Math.floor(Math.random() * (max - min) + min);
+export default props=>{
+  const min = Math.ceil(props.min);
+  const max = Math.floor(props.max);
+  const aleatorio=Math.floor(Math.random()*(max - min) + min);
+  return(
+    <div>
+      <h2>Valor Aleatório</h2>
+      <p><strong>Valor Mínimo: </strong>{min}</p>
+      <p><strong>Valor Máximo: </strong>{max}</p>
+      <p><strong>Valor Escolhido: </strong>{aleatorio}</p>
+    </div>
+  )
 }
