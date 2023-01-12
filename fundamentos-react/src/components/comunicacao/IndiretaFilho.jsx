b77063c3add269b56f2482031960e3c99f4b7b75
Marcio Bueno <mabuen@live.com>
2023-01-12 11:37:38 +0100

    componente com estado

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -2,10 +2,12 @@
 import React from "react"
 
 export default props=>{
+  const gerarIdade=()=>parseInt(Math.random()*20)+50
+  const gerarNerd=()=>Math.random()>.5
   return(
     <div>
       <div>Filho</div>
-      <button onClick={_=>props.quandoClicar("Joao",53,true)}>
+      <button onClick={_=>props.quandoClicar("Joao",gerarIdade(),gerarNerd)}>
         Fornecer informacoes
       </button>
     </div>
